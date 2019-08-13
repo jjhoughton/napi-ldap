50a0989378ceee6ebe5e9027ec3dbaeea64ed47c
Joshua Houghton <joshua.houghton@ripjar.com>
2019-08-13 21:23:08 +0100

    add tests

---
+++ test/run_server.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+SLAPD=/usr/sbin/slapd
+SLAPADD=/usr/sbin/slapadd
+MKDIR=/bin/mkdir
+RM=/bin/rm
+KILL=/bin/kill
+
+$RM -rf openldap-data
+$MKDIR openldap-data
+
+$SLAPADD -f slapd.conf < startup.ldif
+$SLAPD -d999 -f slapd.conf -h "ldap://:1234 ldapi://%2ftmp%2fslapd.sock ldaps://localhost:1235"
+SLAPD_PID=$!
+# slapd should be running now
+
