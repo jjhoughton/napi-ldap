72f5a5c76ca1a4d09944dd14f4cfa42c051f2b0c
James Moxon <jameswmoxon@gmail.com>
2019-09-08 10:06:46 +0100

    fix sasl tests and free defaults in the callback

--- sasl.c
+++ sasl.c
@@ -1,6 +1,7 @@
 #include <node_api.h>
 #include <sasl/sasl.h>
 #include <assert.h>
+#include <stdlib.h>
 #include "cnx.h"
 
 struct sasl_defaults {
@@ -18,21 +19,19 @@ static int sasl_callback(LDAP *ldap_cnx, unsigned flags, void *_defaults, void *
   const char *dflt = interact->defresult;
 
   while(interact->id != SASL_CB_LIST_END) {
-
-
     switch (interact->id) {
-    case SASL_CB_AUTHNAME:
-        dflt = defaults->user;
-        break;
-    case SASL_CB_PASS:
-        dflt = defaults->password;
-        break;
-    case SASL_CB_GETREALM:
-        dflt = defaults->realm;
-        break;
-    case SASL_CB_USER:
-        dflt = defaults->proxy_user;
-        break;
+      case SASL_CB_AUTHNAME:
+          dflt = defaults->user;
+          break;
+      case SASL_CB_PASS:
+          dflt = defaults->password;
+          break;
+      case SASL_CB_GETREALM:
+          dflt = defaults->realm;
+          break;
+      case SASL_CB_USER:
+          dflt = defaults->proxy_user;
+          break;
     }
 
     interact->result = (dflt && *dflt) ? dflt : "";
@@ -40,137 +39,134 @@ static int sasl_callback(LDAP *ldap_cnx, unsigned flags, void *_defaults, void *
     ++interact;
   }
 
+  if (defaults) {
+    if (defaults->mechanism) free(defaults->mechanism);
+    if (defaults->user) free(defaults->user);
+    if (defaults->password) free(defaults->password);
+    if (defaults->realm) free(defaults->realm);
+    if (defaults->proxy_user) free(defaults->proxy_user);
+    if (defaults->sec_props) free(defaults->sec_props);
+
+    free(defaults);
+  }
+
   return LDAP_SUCCESS;
 }
 
-// TODO where do we free defaults
 napi_value
 sasl_bind (napi_env env, napi_callback_info info) {
   napi_status status;
   size_t argc = 6;
   napi_value this, argv[argc], js_ret;
-
   napi_valuetype vt;
 
-  puts("1");
-  int ret;
   struct ldap_cnx *ldap_cnx;
   struct sasl_defaults *defaults = malloc(sizeof(struct sasl_defaults));
 
-  status = napi_get_cb_info (env, info, &argc, NULL, &this, NULL);
+  status = napi_get_cb_info (env, info, &argc, argv, &this, NULL);
   assert (status == napi_ok);
 
   status = napi_unwrap (env, this, (void **) &ldap_cnx);
   assert (status == napi_ok);
 
-  if (argc != 6)
-    {
+  if (argc != 6) {
       napi_throw_error (env, NULL, "This function requires six arguments");
       return NULL;
-    }
+  }
 
   if (ldap_cnx->ld == NULL) {
     napi_throw_error (env, NULL, "LDAP connection has not been established");
     return NULL;
   }
-  puts("2");
+
   size_t size = 0;
 
-    status = napi_typeof (env, argv[0], &vt);
+  status = napi_typeof (env, argv[0], &vt);
+  assert (status == napi_ok);
+
+  if (vt != napi_string) {
+      defaults->mechanism = NULL;
+  } else {
+      status = napi_get_value_string_utf8 (env, argv[0], NULL, 0, &size);
+      assert (status == napi_ok);
+      defaults->mechanism  = malloc (++size);
+      status = napi_get_value_string_utf8 (env, argv[0], defaults->mechanism, size, &size);
+      assert (status == napi_ok);
+  }
+
+  status = napi_typeof (env, argv[1], &vt);
+  assert (status == napi_ok);
+
+  if (vt != napi_string) {
+    defaults->user = NULL;
+  } else {
+    status = napi_get_value_string_utf8 (env, argv[1], NULL, 0, &size);
     assert (status == napi_ok);
-
-    if (vt != napi_string) {
-        defaults->mechanism = NULL;
-    } else {
-        status = napi_get_value_string_utf8 (env, argv[0], NULL, 0, &size);
-        assert (status == napi_ok);
-        defaults->mechanism  = malloc (++size);
-        status = napi_get_value_string_utf8 (env, argv[0], defaults->mechanism, size, &size);
-        assert (status == napi_ok);
-    }
-
-  puts("2.1");
-  puts(argv[1]);
-    status = napi_typeof (env, argv[1], &vt);
+    defaults->user  = malloc (++size);
+    status = napi_get_value_string_utf8 (env, argv[1], defaults->user, size, &size);
     assert (status == napi_ok);
-  puts("2.2");
-    if (vt != napi_string) {
-          puts("2.3");
-        defaults->user = NULL;
-    } else {
-          puts("2.4");
-        status = napi_get_value_string_utf8 (env, argv[1], NULL, 0, &size);
-        assert (status == napi_ok);
-        defaults->user  = malloc (++size);
-        status = napi_get_value_string_utf8 (env, argv[1], defaults->user, size, &size);
-        assert (status == napi_ok);
-    }
-
-  puts("3");
-    status = napi_typeof (env, argv[2], &vt);
-    assert (status == napi_ok);
-
-    if (vt != napi_string) {
-        defaults->password = NULL;
-    } else {
-        status = napi_get_value_string_utf8 (env, argv[2], NULL, 0, &size);
-        assert (status == napi_ok);
-        defaults->password  = malloc (++size);
-        status = napi_get_value_string_utf8 (env, argv[2], defaults-> password, size, &size);
-        assert (status == napi_ok);
-    }
-
-    status = napi_typeof (env, argv[3], &vt);
-    assert (status == napi_ok);
-
-    if (vt != napi_string) {
-        defaults->realm = NULL;
-    } else {
-        status = napi_get_value_string_utf8 (env, argv[3], NULL, 0, &size);
-        assert (status == napi_ok);
-        defaults->realm  = malloc (++size);
-        status = napi_get_value_string_utf8 (env, argv[3], defaults->realm, size, &size);
-        assert (status == napi_ok);
-
-    }
-
-  puts("4");
-    status = napi_typeof (env, argv[4], &vt);
-    assert (status == napi_ok);
-
-    if (vt != napi_string) {
-        defaults->proxy_user = NULL;
-    } else {
-        status = napi_get_value_string_utf8 (env, argv[4], NULL, 0, &size);
-        assert (status == napi_ok);
-        defaults->proxy_user  = malloc (++size);
-        status = napi_get_value_string_utf8 (env, argv[4], defaults->proxy_user, size, &size);
-        assert (status == napi_ok);
-
-    }
-
-    status = napi_typeof (env, argv[5], &vt);
-    assert (status == napi_ok);
+  }
 
 
-    if (vt != napi_string) {
-        defaults->sec_props = NULL;
-    } else {
-        status = napi_get_value_string_utf8 (env, argv[5], NULL, 0, &size);
-        assert (status == napi_ok);
-        defaults->sec_props  = malloc (++size);
-        status = napi_get_value_string_utf8 (env, argv[5], defaults->sec_props, size, &size);
-        assert (status == napi_ok);
+  status = napi_typeof (env, argv[2], &vt);
+  assert (status == napi_ok);
 
-    }
+  if (vt != napi_string) {
+      defaults->password = NULL;
+  } else {
+      status = napi_get_value_string_utf8 (env, argv[2], NULL, 0, &size);
+      assert (status == napi_ok);
+      defaults->password  = malloc (++size);
+      status = napi_get_value_string_utf8 (env, argv[2], defaults-> password, size, &size);
+      assert (status == napi_ok);
+  }
 
-    if (defaults->sec_props) {
-        int res = ldap_set_option(ldap_cnx->ld, LDAP_OPT_X_SASL_SECPROPS, defaults->sec_props);
-        if(res != LDAP_SUCCESS) {
-            napi_throw_error(env, NULL, ldap_err2string(res));
-            return NULL;
-        }
-    }
+  status = napi_typeof (env, argv[3], &vt);
+  assert (status == napi_ok);
+
+  if (vt != napi_string) {
+      defaults->realm = NULL;
+  } else {
+      status = napi_get_value_string_utf8 (env, argv[3], NULL, 0, &size);
+      assert (status == napi_ok);
+      defaults->realm  = malloc (++size);
+      status = napi_get_value_string_utf8 (env, argv[3], defaults->realm, size, &size);
+      assert (status == napi_ok);
+  }
+
+  status = napi_typeof (env, argv[4], &vt);
+  assert (status == napi_ok);
+
+  if (vt != napi_string) {
+      defaults->proxy_user = NULL;
+  } else {
+      status = napi_get_value_string_utf8 (env, argv[4], NULL, 0, &size);
+      assert (status == napi_ok);
+      defaults->proxy_user  = malloc (++size);
+      status = napi_get_value_string_utf8 (env, argv[4], defaults->proxy_user, size, &size);
+      assert (status == napi_ok);
+  }
+
+  status = napi_typeof (env, argv[5], &vt);
+  assert (status == napi_ok);
+
+  if (vt != napi_string) {
+      defaults->sec_props = NULL;
+  } else {
+      status = napi_get_value_string_utf8 (env, argv[5], NULL, 0, &size);
+      assert (status == napi_ok);
+      defaults->sec_props  = malloc (++size);
+      status = napi_get_value_string_utf8 (env, argv[5], defaults->sec_props, size, &size);
+      assert (status == napi_ok);
+  }
+
+  if (defaults->sec_props) {
+      int res = ldap_set_option(ldap_cnx->ld, LDAP_OPT_X_SASL_SECPROPS, defaults->sec_props);
+      if(res != LDAP_SUCCESS) {
+          napi_throw_error(env, NULL, ldap_err2string(res));
+          return NULL;
+      }
+  }
 
 
   int msgid;
@@ -190,7 +186,6 @@ sasl_bind (napi_env env, napi_callback_info info) {
   status = napi_create_int32 (env, msgid, &js_ret);
   assert (status == napi_ok); 
   return js_ret;
-
 }
 
 
